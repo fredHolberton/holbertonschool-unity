ef0c663156d1057f8503019aadf98b71450039fb
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-03-31 12:30:12 +0200

    2. The sound of music

--- unity-audio/Assets/Scripts/MainMenu.cs
+++ unity-audio/Assets/Scripts/MainMenu.cs
@@ -7,10 +7,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 
-{
-    // Audio source to play clip
-    public AudioSource audioSource;
-    
+{ 
     // Start is called before the first frame update
     void Start()
     {
